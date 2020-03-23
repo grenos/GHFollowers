0ee5ad6b43f40292ce32ce0bdaf8595328d7a05e
grenos <grenos>
2020-03-23 17:52:36 +0100

    custom textField component

---
+++ GHFollowers/GFTextField.swift
@@ -0,0 +1,47 @@
+//
+//  GFTextField.swift
+//  GHFollowers
+//
+//  Created by Vasileios Gkreen on 23/03/2020.
+//  Copyright © 2020 Vasileios Gkreen. All rights reserved.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        // custom code
+        configure()
+        
+    }
+    
+    // storyboard init -- not needed --
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // generic styles
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        
+        // .label --> system color that changes automatically with dark/light mode
+        textColor = .label
+        // cursor color
+        tintColor = .label
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+        
+        placeholder = "Enter a username"
+    }
+}
