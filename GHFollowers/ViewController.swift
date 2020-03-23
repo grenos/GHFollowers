0038b5f4ea32f79aa1def9cbf1adb089a11e438f
grenos <grenos>
2020-03-23 15:59:54 +0100

    init without storyboard

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Vasileios Gkreen on 23/03/2020.
+//  Copyright © 2020 Vasileios Gkreen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
