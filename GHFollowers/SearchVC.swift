307765c06c50de86f602c82425d5ce282ac2aa46
grenos <grenos>
2020-03-24 11:17:33 +0100

    add image view, add constraints programatically

--- GHFollowers/SearchVC.swift
+++ GHFollowers/SearchVC.swift
@@ -20,6 +20,7 @@ class SearchVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        configureLogoImageView()
     }
     
     // component will focus
@@ -28,6 +29,26 @@ class SearchVC: UIViewController {
         // hide navbar on this screen
         // called in this lifecycle because we need to hide it everytime we enter this screen
         navigationController?.isNavigationBarHidden = true
+    }
+    
+    
+    
+    func configureLogoImageView() {
+        view.addSubview(logoImageView)
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        logoImageView.image = UIImage(named: "gh-logo")!
+        
+        NSLayoutConstraint.activate([
+            // place it on top after the safeArea and give it a margin of 80
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            // center the image in the view horizontally
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            // set image height
+            logoImageView.heightAnchor.constraint(equalToConstant: 200),
+            // set width
+            logoImageView.widthAnchor.constraint(equalToConstant: 200)
+            
+        ])
     }
     
 }
