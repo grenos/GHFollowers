8599d47f0c457476daa3aa0a2375f4db93419e13
grenos <grenos>
2020-03-24 11:01:07 +0100

    hide navbar in viewWillAppear lifecycle

--- GHFollowers/SearchVC.swift
+++ GHFollowers/SearchVC.swift
@@ -9,10 +9,25 @@
 import UIKit
 
 class SearchVC: UIViewController {
+    
+    // init the elements for the screen
+    let logoImageView = UIImageView()
+    let userNameTextFiled = GFTextField()
+    let CTAButton = GFButton(backgroundColor: .systemGreen, title: "Get Followers")
+    
 
+    // component did mount
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
+        view.backgroundColor = .systemBackground
+    }
+    
+    // component will focus
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        // hide navbar on this screen
+        // called in this lifecycle because we need to hide it everytime we enter this screen
+        navigationController?.isNavigationBarHidden = true
     }
     
 }
