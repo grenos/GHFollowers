53dc1a2ae5f07bbe356c36ad06f96babef8ad67d
grenos <grenos>
2020-03-24 19:23:19 +0100

    add GFAlertContainer

--- GHFollowers/Components/GFAlertVC.swift
+++ GHFollowers/Components/GFAlertVC.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class GFAlertVC: UIViewController {
     
-    let containerView = UIView()
+    let containerView = GFAlertContainerView()
     let titleLabel = GFTitleLabel(textAlignment: .center, fontSize: 20)
     let messageLabel = GFBodyLabel(textAlignment: .center)
     let CTAButton = GFButton(backgroundColor: .systemPink, title: "Ok")
@@ -50,11 +50,6 @@ class GFAlertVC: UIViewController {
     
     func configureContainerView() {
         view.addSubview(containerView)
-        containerView.backgroundColor = .systemBackground
-        containerView.layer.cornerRadius = 16
-        containerView.layer.borderWidth = 2
-        containerView.layer.borderColor = UIColor.white.cgColor
-        containerView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             // center vertically to screen
