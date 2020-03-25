88fe021fdc3b08a286643693441ae06dd01755c8
grenos <grenos>
2020-03-25 18:25:17 +0100

    follower cell setup

---
+++ GHFollowers/Components/Cells/FollowerCell.swift
@@ -0,0 +1,58 @@
+//
+//  FollowerCell.swift
+//  GHFollowers
+//
+//  Created by Vasileios Gkreen on 25/03/2020.
+//  Copyright © 2020 Vasileios Gkreen. All rights reserved.
+//
+
+import UIKit
+
+class FollowerCell: UICollectionViewCell {
+    
+    // this is for the cell to be reusable (virtual list)
+    static let reuseID = "FollowerCell"
+    
+    // declare components
+    let avatarImageView = GFAvatarImageView(frame: .zero)
+    let usernameLabel = GFTitleLabel(textAlignment: .center, fontSize: 16)
+    
+    let padding: CGFloat = 8
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    // setter for the follower data
+    func set(follower: Follower) {
+        usernameLabel.text = follower.login
+    }
+    
+    
+    private func configure() {
+        addSubview(avatarImageView)
+        addSubview(usernameLabel)
+        
+            
+        NSLayoutConstraint.activate([
+            avatarImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: padding),
+            avatarImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            avatarImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            avatarImageView.heightAnchor.constraint(equalTo: avatarImageView.widthAnchor),
+            
+            usernameLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 12),
+            usernameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            usernameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: padding),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 20)
+        ])
+        
+    }
+    
+    
+}
